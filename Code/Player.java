f55bd8014f3b95da6dd1168c9675c9bd917bade8
MagicalGlove <90389752+MagicalGlove@users.noreply.github.com>
2021-11-04 10:56:20 +0100

    Add files via upload

--- Code/Player.java
+++ Code/Player.java
@@ -1,5 +1,52 @@
-package Code;
-
 public class Player {
-    
+    final int playerID;
+
+    private String name;
+    private String schoolClass;
+    private String mail;
+    private String phoneNumber;
+
+    public Player(int playerID, String name, String schoolClass, String mail, String phoneNumber) {
+        this.playerID = playerID;
+        this.name = name;
+        this.schoolClass = schoolClass;
+        this.mail = mail;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchoolClass() {
+        return schoolClass;
+    }
+
+    public void setSchoolClass(String schoolClass) {
+        this.schoolClass = schoolClass;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
