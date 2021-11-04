19be0df56f9dd52fce7c4eaa7645fe819df32004
DenisPedersen <89845900+DenisPedersen@users.noreply.github.com>
2021-11-04 14:10:53 +0100

    Add files via upload

---
+++ Code/User.java
@@ -0,0 +1,19 @@
+public class User {
+
+
+    private void userMenu() {
+        return userMenu;
+    }
+
+    private void showTable() {
+        return table;
+    }
+
+    private void viewTeams() {
+        return teams;
+    }
+
+    private void viewMatchProgramme() {
+        return matchProgramme;
+    }
+}
