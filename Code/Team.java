3f86eae46c1a24477e78d0c646a95b8707baaaf2
H3ZZZZ <89845961+H3ZZZZ@users.noreply.github.com>
2021-11-04 11:45:24 +0100

    Update Team.java

--- Code/Team.java
+++ Code/Team.java
@@ -1,5 +1,38 @@
 package Code;
 
+import java.util.ArrayList;
+
 public class Team {
-    
+
+    private final int teamID;
+    private String teamName;
+    int points;
+    int gamesPlayed;
+    int goalsScored;
+    int goalsConceded;
+
+    ArrayList<Integer> playerIDs = new ArrayList<Integer>();
+    ArrayList<Integer> matchIDs = new ArrayList<Integer>();
+
+    public Team(int teamID, String teamName) {
+        this.teamID = teamID;
+        this.teamName = teamName;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public int getGoalsConceded() {
+        return goalsConceded;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
 }
+
